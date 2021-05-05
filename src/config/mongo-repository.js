6bf16e17a603de2dc6f39c773a26a7762fb848df
trevwhin <twhinmill@clarionat.com>
2021-05-05 15:39:05 +0100

    Implemented environment config which works in dev and production builds

--- src/config/mongo-repository.js
+++ src/config/mongo-repository.js
@@ -1,4 +1,6 @@
 module.exports = {
-  location: `mongodb://${process.env.MONGO_URL || "localhost:27017"}`,
+  location: `mongodb://${process.env.MONGO_HOST || "localhost"}:${
+    process.env.MONGO_PORT || "27017"
+  }`,
   dbName: process.env.DB_NAME || "MyData",
 };
