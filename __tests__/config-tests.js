75f085e2785155fb7c77224cd0315fff3d5324e1
trevwhin <twhinmill@clarionat.com>
2021-05-17 13:51:18 +0100

    Add skeleton unit test to service

---
+++ __tests__/config-tests.js
@@ -0,0 +1,10 @@
+describe("Testing service config modules", () => {
+
+    test("Test Pass", () => {
+        expect(1).toEqual(1);
+    })
+
+    test("Test Fail", () => {
+        expect(1).toEqual(2);
+    })
+});
